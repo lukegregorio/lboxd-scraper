74c520257ee64b14d6ac8c2d282c9684c2eb434b
lukegregorio <lukegregorio99@gmail.com>
2023-10-01 17:36:36 +0100

    add tests

---
+++ tests/main_test.py
@@ -0,0 +1,10 @@
+import src.letterboxd_scraper.main
+
+def test_load_script_tags():
+
+    url = 'https://letterboxd.com/film/parasite-2019/'
+
+    data = src.letterboxd_scraper.main.load_script_tags(url)
+
+    assert data['name'] == 'Parasite'
+    assert data['@type'] == 'Movie'
